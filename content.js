ef994a217f45474639c88a0fd6086d8f231978e9
beridzenika <85935504+beridzenika@users.noreply.github.com>
2026-04-20 21:49:23 +0400

    gpa summary

--- content.js
+++ content.js
@@ -14,36 +14,63 @@ async function getCurrentSubjects() {
   return subjects;
 } 
 
+// row functions
+function gradeClass(g) {
+  if (g === 'A') return 'grade-a';
+  if (g === 'B') return 'grade-b';
+  if (g === 'C') return 'grade-c';
+  if (g === 'F') return 'grade-f';
+}
+
+//main
 async function init() {
-
-  // get data
-  const currentSubjects = await getCurrentSubjects();
-
-  const rows = document.querySelectorAll('tbody')[2].querySelectorAll('tr');
-  const filteredRows = [...rows].filter(tr => tr.querySelector('td input[type="text"]:not(.style4)'));
-
-  const dataRows = filteredRows.map(tr => {
-    const tds = tr.querySelectorAll('td');
-    const submitBtn = tds[1].querySelector('input[type="submit"]');
-    return {
-      semester: tds[0].querySelector('input')?.value,
-      subject:  submitBtn?.value,
-      name:     tds[2].innerText.trim(),
-      credits:  tds[3].innerText.trim(),
-      percent:  tds[4].querySelector('input')?.value,
-      grade:    tds[5].innerText.trim(),
-      score:    tds[6].innerText.trim(),
-      form:     submitBtn,
-      current: currentSubjects.includes(submitBtn?.value),
-    };
-  });
-  dataRows.sort((a,b) => a.subject - b.subject);
-  dataRows.sort((a,b) => b.semester - a.semester);
-  dataRows.sort((a,b) => b.current - a.current);
-
   // clear original
   const mainTable = document.querySelectorAll('tbody')[1].closest('table');
   mainTable.style.display = 'none';
+
+  // get data
+  const currentSubjects = await getCurrentSubjects();
+  const rows = [...document.querySelectorAll('tbody')[2].querySelectorAll('tr')];
+
+  let currSemester = 0;
+  const classified = rows.map(tr => {
+    const tds = tr.querySelectorAll('td');
+    const submitBtn = tds[1].querySelector('input[type="submit"]');
+
+    if (tr.querySelector('input[type="text"]:not(.style4)')) {
+      currSemester = tds[0].querySelector('input')?.value;
+      return {
+        type: 'subject',
+        semester: currSemester,
+        subject:  submitBtn?.value,
+        name:     tds[2].innerText.trim(),
+        credits:  tds[3].innerText.trim(),
+        percent:  tds[4].querySelector('input')?.value,
+        grade:    tds[5].innerText.trim(),
+        score:    tds[6].innerText.trim(),
+        form:     submitBtn,
+        current: currentSubjects.includes(submitBtn?.value),
+      };
+    }
+    if (tr.querySelector('input.style4')) {
+      return {
+        type: 'summary',
+        semester: currSemester,
+        label: tds[0].innerText.trim(),
+        percent: tr.querySelector('input[name^="wpr"]')?.value,
+        credits: tr.querySelector('input[name^="wgpa"]')?.value,
+      }
+    }
+    return {type: 'skip'};
+  })
+  const filteredRows = classified.filter(r => r.type !== 'skip');
+
+  //sort
+  filteredRows.sort((a,b) => a.subject - b.subject);
+  filteredRows.sort((a,b) => b.semester - a.semester);
+  filteredRows.sort((a,b) => b.current - a.current);
+
+ 
 
   // create header
   const container = document.createElement('div');
@@ -69,40 +96,45 @@ async function init() {
   // fill it with your data
   const tbody = document.getElementById('clean-tbody');
 
-  dataRows.forEach(row => {
+  filteredRows.forEach(row => {
     const newTr = document.createElement('tr');
 
-    newTr.innerHTML = `
-      <tr>
-        <td><span class="semester-tag">${row.semester}</span></td>
-        <td><button class="subject-btn">${row.subject}</button></td>
-        <td>${row.name}</td>
-        <td><span class="badge credit">${row.credits}</span></td>
-        <td>${Math.trunc(row.percent)}%</td>
-        <td><span class="badge ${gradeClass(row.grade)}">${row.grade}</span></td>
-        <td>${row.score}</td>
-      </tr>
-    `;
-    newTr.className = row.current ? 'current' : '';
-
-    const btn = newTr.querySelector('.subject-btn');
-    if (row.form) {
-      btn.addEventListener('click', (e) => {
-        e.preventDefault();
-        row.form.click();
-      })
+    if (row.type === 'subject') {
+      newTr.innerHTML = `
+        <tr>
+          <td><span class="semester-tag">${row.semester}</span></td>
+          <td><button class="subject-btn">${row.subject}</button></td>
+          <td>${row.name}</td>
+          <td><span class="badge credit">${row.credits}</span></td>
+          <td>${Math.trunc(row.percent)}%</td>
+          <td><span class="badge ${gradeClass(row.grade)}">${row.grade}</span></td>
+          <td>${row.score}</td>
+        </tr>
+      `;
+      newTr.className = row.current ? 'current' : '';
+      
+      const btn = newTr.querySelector('.subject-btn');
+      if (row.form) {
+        btn.addEventListener('click', (e) => {
+          e.preventDefault();
+          row.form.click();
+        })
+      }
+    }
+    else if (row.type === 'summary') {
+      newTr.innerHTML = `
+        <tr class="summary-row">
+          <td colspan="4" class="summary-lable"><span>${row.label}</span></td>
+          <td>${row.percent}${row.label.includes('პროცენტი')? '%': ''}</td>
+          <td colspan="2">${row.credits}</td>
+        </tr>
+      `;
+      newTr.className = 'summary-row';
     }
 
     tbody.appendChild(newTr);
   });
-
-  // row functions
-  function gradeClass(g) {
-    if (g === 'A') return 'grade-a';
-    if (g === 'B') return 'grade-b';
-    if (g === 'C') return 'grade-c';
-    if (g === 'F') return 'grade-f';
-  }
 }
+
 
 init();
