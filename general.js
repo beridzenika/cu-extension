b7c2c29cdd5ad6e207c23d446433b8910dd6c396
beridzenika <85935504+beridzenika@users.noreply.github.com>
2026-04-28 21:14:35 +0400

    display none issue

--- general.js
+++ general.js
@@ -1,3 +1,8 @@
+const top_space = document.querySelector("body > table > tbody > tr:first-child > td");
+if (top_space && top_space.innerHTML.trim() === "&nbsp;") {
+    top_space.style.display = 'none';
+}
+
 const links = document.querySelectorAll('tbody tr:nth-child(2) td div a');
 links.forEach(link => {
     if(window.location.href === link.href) link.className = "active-link";
